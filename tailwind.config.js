9d395e2bce7e78483e9175a1d94a3d75fbd8b6b5
shriwar <shriwartoshniwal@gmail.com>
2024-08-24 20:09:29 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure this line includes all the file types you use.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
